af178c0ab09210c276234db7c1b6f0128a8407c2
Tufan Oruk <tufan.oruk@gmail.com>
2022-07-09 17:57:16 +0300

    subscription/authorize api and code generation

---
+++ gencode.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+
+generator="openapi-generator"
+
+Usage="\
+Usage: gencode.sh <YAML_specification> <Language> \n
+\tYAML should be OpenAPI 3.0.0 foramat \n
+\tLanguage should be one of 'openapi-generator list'\n 
+"
+
+if [ ! $2 ]
+then 
+    echo -e $Usage
+    exit
+fi
+
+SPEC=$1
+LANG=$2
+
+echo "Generating $LANG code from $SPEC"
+
+$generator validate -i $SPEC
+if [ $? -ne 0 ] 
+then
+    echo "Invalid $SPEC file"
+    exit
+fi
+
+SRC=$LANG"_src"
+
+$generator generate \
+    --api-name-suffix Enkudo_DSP \
+    --generator-name $LANG \
+    --output $SRC \
+    --input-spec $SPEC
+
